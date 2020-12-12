b380917d5793e2e623e453370b140c4cc9989872
ZebulonFeng <jianghaozhang@qq.com>
2020-12-12 22:01:44 +0800

    OOM test

--- chapter3/src/test/TestAllocation.java
+++ chapter3/src/test/TestAllocation.java
@@ -2,29 +2,29 @@ package test;
 
 /**
  * 测试JVM给对象分配内存
+ *
  * VM参数
  * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8 -XX:+PrintGCDetails
  *
- * GC输出日志
- * Heap
- *  PSYoungGen      total 9216K, used 7822K [0x00000000ff600000, 0x0000000100000000, 0x0000000100000000)
- *   eden space 8192K, 95% used [0x00000000ff600000,0x00000000ffda3bb0,0x00000000ffe00000)
- *   from space 1024K, 0% used [0x00000000fff00000,0x00000000fff00000,0x0000000100000000)
- *   to   space 1024K, 0% used [0x00000000ffe00000,0x00000000ffe00000,0x00000000fff00000)
- *  ParOldGen       total 10240K, used 4096K [0x00000000fec00000, 0x00000000ff600000, 0x00000000ff600000)
- *   object space 10240K, 40% used [0x00000000fec00000,0x00000000ff000010,0x00000000ff600000)
- *  PSPermGen       total 21504K, used 3190K [0x00000000f9a00000, 0x00000000faf00000, 0x00000000fec00000)
- *   object space 21504K, 14% used [0x00000000f9a00000,0x00000000f9d1db08,0x00000000faf00000)
+ * GC输出日志(JDK版本 1.8.0_60)
  *
- *   很明显，需要4MB连续内存空间的大对象allocation4直接进入了老年代ParOldGen
- *   疑问：新生代PSYoungGen和永久代PSPermGen的内存分配过程是怎样的？为何会像日志打印的这样？
+ * [GC (Allocation Failure) [PSYoungGen: 6264K->808K(9216K)] 6264K->4912K(19456K), 0.0032876 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+ * Heap
+ *  PSYoungGen      total 9216K, used 7160K [0x00000000ff600000, 0x0000000100000000, 0x0000000100000000)
+ *   eden space 8192K, 77% used [0x00000000ff600000,0x00000000ffc34020,0x00000000ffe00000)
+ *   from space 1024K, 78% used [0x00000000ffe00000,0x00000000ffeca020,0x00000000fff00000)
+ *   to   space 1024K, 0% used [0x00000000fff00000,0x00000000fff00000,0x0000000100000000)
+ *  ParOldGen       total 10240K, used 4104K [0x00000000fec00000, 0x00000000ff600000, 0x00000000ff600000)
+ *   object space 10240K, 40% used [0x00000000fec00000,0x00000000ff002020,0x00000000ff600000)
+ *  Metaspace       used 3263K, capacity 4496K, committed 4864K, reserved 1056768K
+ *   class space    used 357K, capacity 388K, committed 512K, reserved 1048576K
+ *
+ *   很明显，需要4MB连续内存空间的大对象allocation4直接进入了老年代ParOldGen。allocation1 2 3 这三个对象进入了年轻代PSYoungGen
  *
  *   使用  java -XX:+PrintCommandLineFlags -version  命令打印出机器上JVM的各项配置参数，
- *   可以看到 -XX:+UseParallelGC  对于JRE1.7和JRE1.8都是这样，说明它们使用的是Parallel Scavenge和Parallel  Old这
- *   两种GC的组合。（在Server VM模式下）
+ *   可以看到 -XX:+UseParallelGC
  *
- *   而在JRE1.6. （client模式）环境下测试，使用的是serial/serial old组合。
- *   GC输出日志如下：
+ *   对于JRE1.7和JRE1.8，它们使用的是 Parallel Scavenge和 Parallel Old这两种GC的组合。（在Server VM模式（默认）下）
  *
  */
 public class TestAllocation {
@@ -35,12 +35,17 @@ public class TestAllocation {
     private static final int _1MB = 1024 * 1024;
 
     private static void testAllocation() {
-        byte[] allocation1, allocation2, allocation3, allocation4;
+        byte[] allocation1, allocation2, allocation3, allocation4,allocation5;
         allocation1 = new byte[_1MB * 2];
         allocation2 = new byte[_1MB * 2];
         allocation3 = new byte[_1MB * 2];
 
         allocation4 = new byte[_1MB * 4];
+
+        // PSYoungGen      total 9216K, used 5000K
+        // ParOldGen       total 10240K, used 6145K
+        // 年轻代剩余4216k空间，不够放置6MB对象；老年代剩余4095k空间，仍然不够放置6MB对象；所以最终 outOfMemoryError OOM
+        allocation5 = new byte[_1MB * 6];
     }
 
 }
